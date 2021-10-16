7a4225922d3378118126d89493e1c5aa9aa1e5bf
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-10-15 18:27:40 -0700

    Update drivetrain.hpp

--- include/subsystems/drivetrain.hpp
+++ include/subsystems/drivetrain.hpp
@@ -6,7 +6,12 @@ extern Motor leftBack;
 extern Motor leftFront;
 extern Motor rightBack;
 extern Motor rightFront;
+extern ADIEncoder encoder;
 
 void updateDrive();
 
+//void odometry(double x_pos, double y_pos);
+
 void translatePID(double leftDistance, double rightDistance);
+
+void rotate_PID(double turn_degrees);
