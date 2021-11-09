ecbaf7b93f93664d91a6789f497698133648383c
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-11-08 21:06:09 -0800

    Update ports.hpp

--- include/subsystems/ports.hpp
+++ include/subsystems/ports.hpp
@@ -1,21 +1,14 @@
 #include "main.h"
 
 //Drivetrain Ports
-const int rightFrontPort = 6;
+const int rightFrontPort = 9;
 const int rightBackPort = 18;
 const int leftFrontPort = 10;
 const int leftBackPort = 12;
 
 //Lift Ports
-const int itzLiftPort = 4;
 const int fourBarLiftPort = 15;
-
-//Roller intake Port
-const int rollerPort = 16;
+const int twoBarLiftPort = 13;
 
 //Pneumatic Ports
-const char pneumaticLeftPort = 'G';
-
-const int imuPort = 14;
-const char encoderTop = 'E';
-const char encoderBottom = 'F';
+const char pneumaticPort = 'A';
