30244fdec41942a5e4796480bff7c106a7c85663
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-11-17 22:25:29 -0800

    Update fourBarLift.cpp

--- src/subsystems/fourBarLift.cpp
+++ src/subsystems/fourBarLift.cpp
@@ -4,53 +4,32 @@ using namespace okapi;
 
 Motor fourBarLift(fourBarLiftPort, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 
-int liftButtonCount;
-
 typedef struct PID pid;
 
 pid FB;
 
-double fourBarPID(double setpoint)
+void fourBarPID(double setpoint)
 {
-  FB.kP = 0.5;
-  FB.kI = 0;
-  FB.kD = 0.05;
-
-  FB.target = setpoint;
-  FB.error = FB.target - fourBarLift.getPosition();
-  FB.integral += FB.error;
-  FB.derivative = FB.error - FB.prevError;
-  FB.power = FB.kP * FB.error + FB.kI * FB.integral + FB.kD * FB.derivative;
-
-  return FB.power;
-}
-
-void updateFourBarLiftMacro()
-{
-  if (controller.getDigital(ControllerDigital::L1) == 1)
+  while (true)
   {
-    liftButtonCount = 1;
-  }
+    FB.kP = 0.55;
+    FB.kI = 0;
+    FB.kD = 0.05;
 
-  else if(controller.getDigital(ControllerDigital::L2) == 1)
-  {
-    liftButtonCount = 2;
-  }
+    FB.target = setpoint;
+    FB.error = FB.target - fourBarLift.getPosition();
+    FB.integral += FB.error;
+    FB.derivative = FB.error - FB.prevError;
+    FB.power = FB.kP * FB.error + FB.kI * FB.integral + FB.kD * FB.derivative;
 
-  if (controller.getDigital(ControllerDigital::A) == 1)
-  {
-    liftButtonCount = 3;
-  }
+    fourBarLift.moveVelocity(FB.power);
 
-  switch (liftButtonCount)
-  {
-  case 1:
-    fourBarLift.moveVelocity(fourBarPID(-800));
-    break;
-  case 2:
-    fourBarLift.moveVelocity(fourBarPID(50));
-    break;
-  case 3:
-    fourBarLift.moveVelocity(fourBarPID(-350));
+    if (abs(FB.error) < 7)
+    {
+      fourBarLift.setBrakeMode(AbstractMotor::brakeMode::brake);
+      break;
+    }
+
+    pros::delay(10);
   }
 }
