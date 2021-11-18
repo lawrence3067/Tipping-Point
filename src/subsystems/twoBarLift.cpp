d816a5e9b519f1906b3901adf85581379cb6c233
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-11-17 22:26:22 -0800

    Update twoBarLift.cpp

--- src/subsystems/twoBarLift.cpp
+++ src/subsystems/twoBarLift.cpp
@@ -4,46 +4,32 @@ using namespace okapi;
 
 Motor twoBarLift(twoBarLiftPort, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
 
-int twoBarButtonCount;
-
 typedef struct PID pid;
 
 pid TB;
 
-double twoBarPID(double setpoint)
+void twoBarPID(double setpoint)
 {
-  TB.kP = 0.7;
-  TB.kI = 0;
-  TB.kD = 0.05;
-
-  TB.target = setpoint;
-  TB.error = TB.target - twoBarLift.getPosition();
-  TB.integral += TB.error;
-  TB.derivative = TB.error - TB.prevError;
-  TB.power = TB.kP * TB.error + TB.kI * TB.integral + TB.kD * TB.derivative;
-
-  return TB.power;
-}
-
-void updateTwoBarLiftMacro()
-{
-  if (controller.getDigital(ControllerDigital::R1) == 1)
+  while (true)
   {
-    twoBarButtonCount = 2;
-  }
-  else if(controller.getDigital(ControllerDigital::R2) == 1)
-  {
-    twoBarButtonCount = 1;
-  }
+    TB.kP = 0.5;
+    TB.kI = 0.001;
+    TB.kD = 0.05;
 
-  switch (twoBarButtonCount)
-  {
-  case 1:
-    twoBarLift.moveVelocity(twoBarPID(-340));
-    break;
-  case 2:
-    twoBarLift.moveVelocity(twoBarPID(-100));
-    break;
-  }
+    TB.target = setpoint;
+    TB.error = TB.target - twoBarLift.getPosition();
+    TB.integral += TB.error;
+    TB.derivative = TB.error - TB.prevError;
+    TB.power = TB.kP * TB.error + TB.kI * TB.integral + TB.kD * TB.derivative;
 
+    twoBarLift.moveVelocity(TB.power);
+
+    if (abs(TB.error) < 7)
+    {
+      twoBarLift.setBrakeMode(AbstractMotor::brakeMode::hold);
+      break;
+    }
+
+    pros::delay(10);
+  }
 }
