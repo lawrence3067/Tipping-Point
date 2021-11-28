e5b188de0b0afa187859603ead3b2c7a08e758dc
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-11-27 21:55:38 -0800

    Update autonomous.cpp

--- src/subsystems/autonomous.cpp
+++ src/subsystems/autonomous.cpp
@@ -2,58 +2,34 @@
 
 using namespace okapi;
 
-void skillsHard()
+void rightAuton()
 {
-  twoBarPID(-340);
-  translatePID(-9);
-  twoBarPID(10);
-  //translatePID(8, 8);
-  pros::delay(500);
-  rotatePID(90);
-  pros::c::adi_digital_write(pneumaticPort, HIGH);
-  translatePID(105);
-  pros::delay(500);
-  translatePID(-10);
-  rotatePID(-90);
-  twoBarPID(-340);
-  translatePID(12);
-  pros::delay(500);
   pros::c::adi_digital_write(pneumaticPort, LOW);
   pros::delay(500);
-  rotatePID(30);
-  translatePID(-75); //130
-  rotatePID(45);
-  translatePID(-30);
+  twoBarLift.moveVelocity(twoBarPID(-340));
+  translatePID(-65);
+  twoBarLift.moveVelocity(twoBarPID(-100));
+  pros::delay(1000);
+  translatePID(40);
+  jankRotatePID(-50, 50);
+  twoBarLift.moveVelocity(twoBarPID(-340));
+  pros::delay(500);
+  jankFourBarPID(-400);
   translatePID(10);
-  rotatePID(-30);
-  translatePID(-50);
-  twoBarPID(10);
-  /**
-  translatePID(8, 8);
-  rotatePID(-30);
-  translatePID(-8, -8);
-
-**/
-  // rotatePID(30);
-  // translatePID(62, 62);
-  // fourBarPID(-800);
-  // rotatePID(-16);
-  // translatePID(7, 7);
-  // fourBarPID(-600);
-  // pros::c::adi_digital_write(pneumaticPort, HIGH);
-
-
-
-  /**
-  rotatePID(55);
-  twoBarPID(-340);
-  translatePID(36, 36);
-  rotatePID(90);
-  translatePID(-12, -12);
-**/
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
 }
 
-void skillsBackup()
+void leftAuton()
 {
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  translatePID(75);
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  pros::delay(1000);
+  translatePID(-55);
+  jankRotatePID(-80,0);
+  twoBarLift.moveVelocity(-340);
+  translatePID(-10);
+  twoBarLift.moveVelocity(0);
+  translatePID(30);
 
 }
