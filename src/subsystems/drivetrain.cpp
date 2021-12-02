afa49e4cb0319b9f76ea1bd16aa405253b7954ba
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-01 20:59:31 -0800

    Update drivetrain.cpp

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -14,85 +14,98 @@ std::shared_ptr<ChassisController> drive =
   .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})
   .build();
 
-typedef struct PID pid;
+  typedef struct PID pid;
 
-pid translate;
-pid rotate;
-pid jankLeft;
-pid jankRight;
+  pid translate;
+  pid rotate;
+  pid jankLeft;
+  pid jankRight;
 
-double inertial_values;
-double error;
-double threshold;
-double integral;
-double derivative;
-double prevError;
-double kP;
-double ki;
-double kd;
-double p;
-double i;
-double d;
-double vel;
-double inertial_value;
+  double inertial_values;
+  double error;
+  double threshold;
+  double integral;
+  double derivative;
+  double prevError;
+  double kP;
+  double ki;
+  double kd;
+  double p;
+  double i;
+  double d;
+  double vel;
+  double inertial_value;
+  double derivative1;
+  double derivative2;
 
-void translatePID(double distance)
+  int timer;
+  int brakeMode;
+
+void updateDrive()
 {
+  drive -> getModel() -> tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY));
+
+  if (controller.getDigital(ControllerDigital::Y) == 1)
+  {
+    rightFront.setBrakeMode(AbstractMotor::brakeMode::coast);
+    rightBack.setBrakeMode(AbstractMotor::brakeMode::coast);
+    leftFront.setBrakeMode(AbstractMotor::brakeMode::coast);
+    leftBack.setBrakeMode(AbstractMotor::brakeMode::coast);
+  }
+
+}
+
+
+void translatePID(double distance, int ms)
+{
+  timer = 0;
   inertial_sensor.reset();
 	drive -> getModel() -> setEncoderUnits(AbstractMotor::encoderUnits::degrees);
 
 	translate.target = distance * (360 / (2 * 3.1415 * (4 / 2)));
 
 	//PID constants
-	translate.kP = 0.000575; //.002075
-	translate.kI = 0.05; //0.05
+	translate.kP = 0.0045;
+	translate.kI = 0;
 	translate.kD = 0.0002;
 
 	auto translateController = IterativeControllerFactory::posPID(translate.kP, translate.kI, translate.kD);
 
 	drive -> getModel() -> resetSensors();
 
-	while (true)
+	while (timer < ms)
 	{
 
     inertial_value = inertial_sensor.get();
 		translate.error = translate.target - ((drive -> getModel() -> getSensorVals()[0] + drive -> getModel() -> getSensorVals()[1])/2 * 18 / 35);
 		translate.power = translateController.step(translate.error);
 
-    if (inertial_value > 0 && distance > 0)
-    {
-      drive -> getModel() -> tank(-translate.power, abs(translate.power) + inertial_value * 0.012);
-    }
-    else if (inertial_value < 0 && distance > 0)
-    {
-      drive -> getModel() -> tank(abs(translate.power) + inertial_value * 0.012, -translate.power);
-    }
-    else
-    {
-      drive -> getModel() -> tank(-translate.power, -translate.power);
-    }
+    drive -> getModel() -> tank(-translate.power, -translate.power);
 
-		if (abs(translate.error) < 12)
+    if (abs(translate.error) < 6  && (rightFront.getActualVelocity() < 0.5 && rightBack.getActualVelocity() < 0.5 && leftFront.getActualVelocity() < 0.5 && leftBack.getActualVelocity() < 0.5))
 		{
 			break;
 		}
 
+    timer += 10;
 		pros::delay(10);
 	}
 
 	drive -> getModel() -> tank(0, 0);
 }
 
-void rotatePID(double target)
+void rotatePID(double target, int ms)
 {
   inertial_sensor.reset();
-  error = target - inertial_sensor.get();
-  threshold = 2; //2
-  kP = 1.6; //0.8
-  ki = 0.02;
-  kd = 0.03; //3.5
+  timer = 0;
+  error = target;
+  threshold = 3;
+  kP = 0.016;  //0.0158
+  ki = 0.00003;
+  kd = 0.0084;
 
-  while (true)
+
+  while (timer < ms)
   {
     error = target - inertial_sensor.get();
     integral = integral + error;
@@ -104,53 +117,15 @@ void rotatePID(double target)
 
     vel = p + i + d;
 
-    rightFront.moveVelocity(-vel);
-    leftFront.moveVelocity(vel);
-    rightBack.moveVelocity(-vel);
-    leftBack.moveVelocity(vel);
+    drive -> getModel() -> tank(vel, -vel);
 
-    if(abs(error) < threshold)
+    if(abs(error) < threshold && rightFront.getActualVelocity() < 1 && rightBack.getActualVelocity() < 1 && leftFront.getActualVelocity() < 1 && leftBack.getActualVelocity() < 1)
     {
       break;
     }
 
+    timer += 10;
     pros::delay(10);
+
   }
-  drive -> getModel() -> tank(0, 0);
-
-}
-
-void jankRotatePID(double leftDistance, double rightDistance)
-{
-  jankLeft.target = leftDistance * 360 / (4 * M_PI);
-  jankRight.target - rightDistance * 360 / (4 * M_PI);
-
-  jankLeft.kP = 0.000775;
-  jankLeft.kI = 0.05;
-  jankLeft.kD = 0.0004;
-
-  auto jankLeftController = IterativeControllerFactory::posPID(jankLeft.kP, jankLeft.kI, jankLeft.kD);
-  auto jankRightController = IterativeControllerFactory::posPID(jankLeft.kP, jankLeft.kI, jankLeft.kD);
-
-  drive -> getModel() -> resetSensors();
-
-  while (true)
-  {
-    jankLeft.error = jankLeft.target - drive -> getModel() -> getSensorVals()[0];
-    jankRight.error = jankRight.target - drive -> getModel() -> getSensorVals()[1];
-
-    jankLeft.power = jankLeftController.step(jankLeft.error);
-    jankRight.power = jankRightController.step(jankRight.error);
-
-    drive -> getModel() -> tank(-jankLeft.power, -jankRight.power);
-
-    if (abs(jankLeft.error) < 1 && abs(jankRight.error) < 1)
-    {
-      break;
-    }
-    pros::delay(10);
-  }
-
-  drive -> getModel() -> tank(0, 0);
-
 }
