f1d28615a71c6e12a7296656f2641d6279f27ea2
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-01 20:59:18 -0800

    Update autonomous.cpp

--- src/subsystems/autonomous.cpp
+++ src/subsystems/autonomous.cpp
@@ -4,32 +4,68 @@ using namespace okapi;
 
 void rightAuton()
 {
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  translatePID(65, 2000);
   pros::c::adi_digital_write(pneumaticPort, LOW);
   pros::delay(500);
-  twoBarLift.moveVelocity(twoBarPID(-340));
-  translatePID(-65);
-  twoBarLift.moveVelocity(twoBarPID(-100));
-  pros::delay(1000);
-  translatePID(40);
-  jankRotatePID(-50, 50);
-  twoBarLift.moveVelocity(twoBarPID(-340));
-  pros::delay(500);
-  jankFourBarPID(-400);
-  translatePID(10);
-  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  translatePID(-40, 2000);
+  rotatePID(90, 1000);
+  rotatePID(48, 700);
+  autonTwoBarPID(-340);
+  translatePID(-65, 2500);
+  autonTwoBarPID(-100);
+  translatePID(70, 4000);
 }
 
 void leftAuton()
 {
   pros::c::adi_digital_write(pneumaticPort, HIGH);
-  translatePID(75);
+  translatePID(65, 2100);
   pros::c::adi_digital_write(pneumaticPort, LOW);
-  pros::delay(1000);
-  translatePID(-55);
-  jankRotatePID(-80,0);
-  twoBarLift.moveVelocity(-340);
-  translatePID(-10);
-  twoBarLift.moveVelocity(0);
-  translatePID(30);
+  pros::delay(500);
+  translatePID(-40, 2000);
+  rotatePID(105, 1500);
+  rotatePID(104, 1500);
+  autonTwoBarPID(-340);
+  translatePID(-60, 2000);
+  autonTwoBarPID(-100);
+  translatePID(60, 2000);
+}
+
+void rightAutonWP()
+{
+  /**
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  autonTwoBarPID(-340);
+  translatePID(-68, 2000);
+  autonTwoBarPID(-50);
+  translatePID(35, 1000);
+  rotatePID(-100, 1000);
+  translatePID(25, 700);
+  autonFourBarPID(-100);
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  autonFourBarPID(0);
+  translatePID(10, 500);
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  pros::delay(500);
+  translatePID(-40, 2000);**/
+
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  autonFourBarPID(-400);
+  translatePID(20, 700);
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  autonFourBarPID(0);
+  translatePID(10, 500);
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  autonFourBarPID(-800);
+  rotatePID(45, 1000);
+  rotatePID(45, 1000);
+  autonFourBarPID(-10);
+  pros::c::adi_digital_write(pneumaticPort, HIGH);
+  autonTwoBarPID(-340);
+  translatePID(-70, 2000);
+  autonTwoBarPID(-50);
+  translatePID(70, 3000);
 
 }
