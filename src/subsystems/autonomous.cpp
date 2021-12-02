1876a8140d115174db01680d5a724fe6b1269e21
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-01 20:59:54 -0800

    Update autonomous.cpp

--- src/subsystems/autonomous.cpp
+++ src/subsystems/autonomous.cpp
@@ -2,70 +2,140 @@
 
 using namespace okapi;
 
-void rightAuton()
+void skillsHard()
 {
+  twoBarPID(-340);
+  translatePID(-7, 500);
+  twoBarPID(-50);
+  rotatePID(90, 1500);
   pros::c::adi_digital_write(pneumaticPort, HIGH);
-  translatePID(65, 2000);
+  translatePID(55, 1500);
   pros::c::adi_digital_write(pneumaticPort, LOW);
   pros::delay(500);
-  translatePID(-40, 2000);
-  rotatePID(90, 1000);
-  rotatePID(48, 700);
-  autonTwoBarPID(-340);
-  translatePID(-65, 2500);
-  autonTwoBarPID(-100);
-  translatePID(70, 4000);
-}
-
-void leftAuton()
-{
-  pros::c::adi_digital_write(pneumaticPort, HIGH);
-  translatePID(65, 2100);
+  rotatePID(45, 700);/**
+  twoBarPID(-340);
+  translatePID(12);
+  pros::delay(500);
   pros::c::adi_digital_write(pneumaticPort, LOW);
   pros::delay(500);
-  translatePID(-40, 2000);
-  rotatePID(105, 1500);
-  rotatePID(104, 1500);
-  autonTwoBarPID(-340);
-  translatePID(-60, 2000);
-  autonTwoBarPID(-100);
-  translatePID(60, 2000);
-}
+  rotatePID(30);
+  translatePID(-75); //130
+  rotatePID(45);
+  translatePID(-30);
+  translatePID(10);
+  rotatePID(-30);
+  translatePID(-50);
+  twoBarPID(10);
+  translatePID(8, 8);
+  rotatePID(-30);
+  translatePID(-8, -8);
 
-void rightAutonWP()
-{
+**/
+  // rotatePID(30);
+  // translatePID(62, 62);
+  // fourBarPID(-800);
+  // rotatePID(-16);
+  // translatePID(7, 7);
+  // fourBarPID(-600);
+  // pros::c::adi_digital_write(pneumaticPort, HIGH);
+
+
+
   /**
-  pros::c::adi_digital_write(pneumaticPort, LOW);
-  autonTwoBarPID(-340);
-  translatePID(-68, 2000);
-  autonTwoBarPID(-50);
-  translatePID(35, 1000);
-  rotatePID(-100, 1000);
-  translatePID(25, 700);
-  autonFourBarPID(-100);
+  rotatePID(55);
+  twoBarPID(-340);
+  translatePID(36, 36);
+  rotatePID(90);
+  translatePID(-12, -12);
+**/
+}
+
+void skillsPush()
+{
   pros::c::adi_digital_write(pneumaticPort, HIGH);
-  autonFourBarPID(0);
-  translatePID(10, 500);
+  translatePID(120, 9000);
+
+  translatePID(-10, 1000);
+  rotatePID(35, 1000);
+  twoBarPID(-340);
+
+  translatePID(-65, 4000);
+  pros::delay(5);
+  translatePID(-25, 2500);
+  //twoBarPID(-340);
+  //translatePID(20);
+
+  // translatePID(-90);
+  // translatePID(20); //20
+
+  pros::delay(5);
+  rotatePID(-85, 2000);
+  translatePID(70, 7000);
+  /**
+  jankRotatePID(80, 0);
+  translatePID(-70);
+  translatePID(20);
+  jankRotatePID(0,50);
+  translatePID(55);
+  pros::delay(500);
   pros::c::adi_digital_write(pneumaticPort, LOW);
   pros::delay(500);
-  translatePID(-40, 2000);**/
+  twoBarPID(10);
+  jankRotatePID(40, 0);
+  translatePID(-100);
+  jankRotatePID(-90, 0);**/
+}
 
+void skillsPlatform()
+{
   pros::c::adi_digital_write(pneumaticPort, HIGH);
-  autonFourBarPID(-400);
-  translatePID(20, 700);
-  pros::c::adi_digital_write(pneumaticPort, LOW);
+  translatePID(98, 3000);
+  fourBarPID(-50);
+  pros::c::adi_digital_write(pneumaticPort, LOW); //clamps blue
+  fourBarPID(-800);
+  rotatePID(86, 1500);
+  translatePID(-15, 700);
+  fourBarPID(0);
+  translatePID(-57, 2500); //pushes rings out the way
+  fourBarPID(-800);
+  rotatePID(-90, 1500);
+  translatePID(18, 700);
+  fourBarPID(-600);
+  pros::delay(500);
   pros::c::adi_digital_write(pneumaticPort, HIGH);
-  autonFourBarPID(0);
-  translatePID(10, 500);
-  pros::c::adi_digital_write(pneumaticPort, LOW);
-  autonFourBarPID(-800);
-  rotatePID(45, 1000);
-  rotatePID(45, 1000);
-  autonFourBarPID(-10);
-  pros::c::adi_digital_write(pneumaticPort, HIGH);
-  autonTwoBarPID(-340);
-  translatePID(-70, 2000);
-  autonTwoBarPID(-50);
-  translatePID(70, 3000);
+  fourBarPID(-800);
+  translatePID(-10, 700);
+  fourBarPID(0);
+  translatePID(-75, 3000); //pushes large netural
+  rotatePID(-75, 2000);
+  translatePID(-100, 2000);
+  translatePID(35, 1500);
+  rotatePID(82, 1500);
+  translatePID(-100, 2000);
+  translatePID(90, 3000);
+  pros::c::adi_digital_write(pneumaticPort, LOW); //clamps small neutral
+  pros::delay(500);
+  rotatePID(-45, 1000);
+  fourBarPID(-800);
+  translatePID(50, 1500);
+  pros::delay(500);
+  fourBarPID(-700);
+  pros::c::adi_digital_write(pneumaticPort, HIGH); //put small neutral on platform
+  translatePID(-10, 1000);
+  rotatePID(110, 2000);
+  fourBarPID(0);
+  twoBarPID(-340);
+  translatePID(-100, 4000);
+  twoBarPID(-100);
+  translatePID(17, 1000);
+  rotatePID(90, 1500);
+  translatePID(200, 10000);
 
+}
+
+
+void test()
+{
+  pros::c::adi_digital_write(pneumaticPort, LOW);
+  pros::lcd::set_text(4, "fuck you");
 }
