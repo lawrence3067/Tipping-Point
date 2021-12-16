261d3ebf786116171f155ee72626b9faa227aa18
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-16 12:33:34 -0800

    Add files via upload

---
+++ include/subsystems/fourBar.hpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+using namespace okapi;
+
+extern Motor fourBarLift;
+
+void fourBarMacros(double setpoint);
+void updateFourBar();
---
+++ include/subsystems/ports.hpp
@@ -0,0 +1,31 @@
+#include "main.h"
+
+//drivetrain ports
+const int rightFrontPort = 0;
+const int rightTopPort = 0;
+const int rightBottomPort = 0;
+
+const int leftFrontPort = 0;
+const int leftTopPort = 0;
+const int leftBottomPort = 0;
+
+//lift port
+const int fourBarPort = 0;
+
+//conveyor port
+const int conveyorPort = 0;
+
+//tilter pneumatic port
+const char rightTilterPort = 'A';
+const char leftTilterPort = 'B';
+
+//pneumatic clamp ports
+const char tilterClampPort = 'C';
+const char fourBarClampPort = 'D';
+
+//encoder ports
+const char leftEncoder1 = 'E';
+const char leftEncoder2 = 'F';
+
+const char rightEncoder1 = 'G';
+const char rightEncoder2 = 'H';
