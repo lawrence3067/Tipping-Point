5dc1e089a8dc40e34711ba47caa88c5af4382e74
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-16 12:32:24 -0800

    Update drivetrain.hpp

--- include/subsystems/drivetrain.hpp
+++ include/subsystems/drivetrain.hpp
@@ -1,3 +1,13 @@
 #include "main.h"
 
-void joystickDrive();
+using namespace okapi;
+
+extern Motor rightFront;
+extern Motor rightTop;
+extern Motor rightBottom;
+
+extern Motor leftFront;
+extern Motor leftTop;
+extern Motor leftBottom;
+
+void updateDrive();
