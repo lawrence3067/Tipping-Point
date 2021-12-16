4c1b2440cfe90f4b1cd873ab8f8cbfeaf33131ce
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-12-16 12:29:23 -0800

    Update drivetrain.cpp

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -2,17 +2,31 @@
 
 using namespace okapi;
 
-/*
-std::shared_ptr<ChassisController> drive =
-  ChassisControllerBuilder()
-  .withMotors({leftBackDrive, leftFrontDrive}, {rightFrontDrive, rightBackDrive})   //MotorGroups for left and right side
-  .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})		  //Blue gearset(100 rpm) and wheel dimensions
-  .build();
+Motor rightFront(rightFrontPort, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightTop(rightTopPort, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightBottom(rightBottomPort, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
 
-void joystickDrive()
+Motor leftFront(leftFrontPort, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor leftTop(leftTopPort, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor leftBottom(leftBottomPort, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+
+ADIEncoder leftEncoder(leftEncoder1, leftEncoder2, false);
+ADIEncoder rightEncoder(rightEncoder1, rightEncoder2, true);
+
+std::shared_ptr<OdomChassisController> drive =
+  ChassisControllerBuilder()
+  .withMotors({leftFront, leftTop, leftBottom}, {rightFront, rightTop, rightBottom})
+  .withDimensions(AbstractMotor::gearset::blue, {{4_in, 13.5_in}, imev5BlueTPR})
+  .withSensors(leftEncoder, rightEncoder)
+  .withOdometry({{3.25_in, 2.8_in}, quadEncoderTPR}, StateMode::CARTESIAN)
+  .withGains(
+      {0, 0, 0},
+      {0, 0, 0},
+      {0, 0, 0}
+  )
+  .buildOdometry();
+
+void updateDrive()
 {
-  drive -> getModel() -> tank(controller.getAnalog(ControllerAnalog::leftY),
-                              controller.getAnalog(ControllerAnalog::rightY));
-  pros::lcd::set_text(3, "hello");
+  drive -> getModel() -> tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY));
 }
-**/
