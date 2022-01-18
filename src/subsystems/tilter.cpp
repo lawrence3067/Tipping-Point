3a7b2a256f5f183823f91d78b22e433d015e88de
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-01-17 17:23:29 -0800

    Update tilter.cpp

--- src/subsystems/tilter.cpp
+++ src/subsystems/tilter.cpp
@@ -4,12 +4,13 @@ void updateTilter()
 {
   if (controller.getDigital(ControllerDigital::X) == 1)
   {
-    pros::c::adi_digital_write(rightTilterPort, HIGH);
-    pros::c::adi_digital_write(leftTilterPort, HIGH);
+    pros::c::adi_digital_write(tilterClampPort, HIGH);
+    pros::delay(250);
+    pros::c::adi_digital_write(tilterPort, LOW);
   }
   else if (controller.getDigital(ControllerDigital::B) == 1)
   {
-    pros::c::adi_digital_write(rightTilterPort, LOW);
-    pros::c::adi_digital_write(leftTilterPort, LOW);
+    pros::c::adi_digital_write(tilterPort, HIGH);
+    pros::c::adi_digital_write(tilterClampPort, LOW);
   }
 }
