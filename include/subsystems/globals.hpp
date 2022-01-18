8cb9a3e70f1e8b9f73e515b26fc2dd4522fdd9c6
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-01-17 17:25:27 -0800

    Update globals.hpp

--- include/subsystems/globals.hpp
+++ include/subsystems/globals.hpp
@@ -3,3 +3,17 @@
 using namespace okapi;
 
 extern Controller controller;
+
+struct PID
+{
+  float kP;
+  float kI;
+  float kD;
+  float target;
+  float error;
+  float prevError;
+  float integral;
+  float derivative;
+  float power;
+  float speed;
+};
