5fef59f32688d3d631f8e0d83f9b9c6f56cfa5e6
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-01-17 17:22:55 -0800

    Update fourBar.cpp

--- src/subsystems/fourBar.cpp
+++ src/subsystems/fourBar.cpp
@@ -2,26 +2,20 @@
 
 using namespace okapi;
 
-Motor fourBarLift(fourBarPort, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+Motor fourBarLift(fourBarPort, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
 
 int liftState;
 double fourBarPower;
 double error;
-auto fourBarController = IterativeControllerFactory::posPID(0.85, 0, 0.05);;
+auto fourBarController = IterativeControllerFactory::posPID(1.5, 0, 0.001);
 
-void fourBarMacros(double setpoint)
+double fourBarMacros(double setpoint)
 {
+  fourBarController.setOutputLimits(-100, 100);
   error = setpoint - fourBarLift.getPosition();
   fourBarPower = fourBarController.step(error);
 
-  if (fourBarController.isSettled() == true)
-  {
-    fourBarLift.moveVelocity(10);
-  }
-  else
-  {
-    fourBarLift.moveVelocity(fourBarPower);
-  }
+  return (-fourBarPower);
 }
 
 void updateFourBar()
@@ -34,14 +28,27 @@ void updateFourBar()
   {
     liftState = 2;
   }
+  else if (controller.getDigital(ControllerDigital::A) == 1)
+  {
+    liftState = 3;
+  }
+  else if (controller.getDigital(ControllerDigital::Y) == 1)
+  {
+    liftState = 4;
+  }
 
   switch (liftState)
   {
     case 1:
-      fourBarLift.moveVelocity(100);
+      fourBarLift.moveVelocity(fourBarMacros(700));
       break;
     case 2:
-      fourBarLift.moveVelocity(-100);
+      fourBarLift.moveVelocity(fourBarMacros(-20));
       break;
+    case 3:
+      fourBarLift.moveVelocity(fourBarMacros(500));
+      break;
+    case 4:
+      fourBarLift.moveVelocity(fourBarMacros(550));
   }
 }
