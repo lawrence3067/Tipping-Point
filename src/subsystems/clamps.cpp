f8e62878f16b6414d5431e33a33fff30085256bc
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-01-17 17:21:33 -0800

    Update clamps.cpp

--- src/subsystems/clamps.cpp
+++ src/subsystems/clamps.cpp
@@ -2,24 +2,12 @@
 
 void updateFourBarClamp()
 {
-  if (controller.getDigital(ControllerDigital::up) == 1)
+  if (controller.getDigital(ControllerDigital::down) == 1)
   {
     pros::c::adi_digital_write(fourBarClampPort, HIGH);
   }
-  else if (controller.getDigital(ControllerDigital::down) == 1)
+  else if (controller.getDigital(ControllerDigital::up) == 1)
   {
     pros::c::adi_digital_write(fourBarClampPort, LOW);
-  }
-}
-
-void updateTilterClamp()
-{
-  if (controller.getDigital(ControllerDigital::left) == 1)
-  {
-    pros::c::adi_digital_write(tilterClampPort, HIGH);
-  }
-  else if (controller.getDigital(ControllerDigital::right) == 1)
-  {
-    pros::c::adi_digital_write(tilterClampPort, LOW);
   }
 }
