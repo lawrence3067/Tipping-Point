703a89e25197b5cc3eabdb911ed6eac5acc3c9fd
Fatimah Hussain <67985868+anony-mouse1@users.noreply.github.com>
2022-01-25 21:45:58 -0800

    Update main.h

--- include/main.h
+++ include/main.h
@@ -75,13 +75,14 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
- #include "subsystems/drive.hpp"
+ #include "subsystems/drivetrain.hpp"
  #include "subsystems/fourBar.hpp"
  #include "subsystems/tilter.hpp"
  #include "subsystems/clamps.hpp"
  #include "subsystems/conveyor.hpp"
  #include "subsystems/ports.hpp"
  #include "subsystems/globals.hpp"
+ #include "motion/QVector.hpp"
 //#include <iostream>
 #endif
 
