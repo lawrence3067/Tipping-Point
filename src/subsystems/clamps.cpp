1d4f9bad8d9e562584cebf71922a3b2e2ff74d1c
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-01-27 20:39:03 -0800

    Update clamps.cpp

--- src/subsystems/clamps.cpp
+++ src/subsystems/clamps.cpp
@@ -1,13 +1,17 @@
 #include "main.h"
 
+pros::ADIDigitalOut fourBarClamp(fourBarClampPort);
+
+pros::ADIDigitalIn fourBarSwitch(fourBarSwitchPort);
+
 void updateFourBarClamp()
 {
-  if (controller.getDigital(ControllerDigital::down) == 1)
+  if (controller.getDigital(ControllerDigital::down) == 1 || fourBarSwitch.get_value() == 1)
   {
-    pros::c::adi_digital_write(fourBarClampPort, HIGH);
+    fourBarClamp.set_value(true);
   }
   else if (controller.getDigital(ControllerDigital::up) == 1)
   {
-    pros::c::adi_digital_write(fourBarClampPort, LOW);
+    fourBarClamp.set_value(false);
   }
 }
