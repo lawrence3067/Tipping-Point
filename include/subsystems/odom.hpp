f89d57b131bc39161097000ff80ef5281ba89bef
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2022-02-02 20:43:47 -0800

    Update odom.hpp

--- include/subsystems/odom.hpp
+++ include/subsystems/odom.hpp
@@ -7,13 +7,12 @@ namespace odom
   extern QVector<QLength> pos;
   extern QVector<Number> heading;
   extern QAngle curAngle;
-  extern double rPrev;
-  extern double lPrev;
+	extern double rPrev;
+	extern double lPrev;
 
   extern void updateOdom();
-  extern void driveToPoint(Point target, bool driveBack);
+  extern void driveToPoint(Point target, bool driveBack, int ms);
   extern void odomRotate(QAngle targetAngle);
-  extern void odomTranslate(QLength targetDistance, bool driveBack);
+  extern void odomTranslate(QLength targetDistance, bool driveBack, int ms);
   extern void odomDrift();
 }
-
